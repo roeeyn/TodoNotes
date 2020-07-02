03145d70d068ebe8ac99d55fc794478006f9d9f6
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-07-02 18:12:40 -0500

    Added frontend

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_HOST")
-db_schema = os.getenv("DB_SCHEMA")
+db_user = os.getenv("DB_USER", "root")
+db_password = os.getenv("DB_PASSWORD", "root")
+db_host = os.getenv("DB_HOST", "localhost")
+db_schema = os.getenv("DB_SCHEMA", "TODO_NOTES")
 
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_schema}"
--- backend/main.py
+++ backend/main.py
@@ -51,6 +51,7 @@ async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db),
 ):
+    print(form_data.username, form_data.password)
     user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
